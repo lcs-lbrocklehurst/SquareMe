1165131bae25af371616a4183b6ee93b5641b5eb
lbrocklehurst <lbrocklehurst@lcs.on.ca>
2023-02-02 14:11:12 -0500

    added  navigation title

---
+++ SquareMe/Views/CalculationView.swift
@@ -0,0 +1,27 @@
+//
+//  CalculationView.swift
+//  SquareMe
+//
+//  Created by Lewis Brocklehurst on 2023-02-02.
+//
+
+import SwiftUI
+
+struct CalculationView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("hello world")
+            }
+            .navigationTitle("Square me")
+        }
+        
+    }
+     
+}
+
+struct CalculationView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalculationView()
+    }
+}
